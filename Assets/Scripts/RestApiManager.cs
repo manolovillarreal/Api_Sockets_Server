3e66a8ada99f861d0f6876857959ad932715a65b
Manuel Villarreal <manolovillarreal@hotmail.com>
2022-05-13 11:10:39 -0500

    first commit

---
+++ Assets/Scripts/RestApiManager.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RestApiManager : MonoBehaviour
+{
+
+    string Token;
+    string Username;
+
+    [SerializeField]
+    private string URL;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitToken();
+    }
+
+    private void InitToken()
+    {
+        Token = PlayerPrefs.GetString("token");
+        Username = PlayerPrefs.GetString("username");
+
+        if (string.IsNullOrEmpty(Token))
+        {
+            Debug.Log("No hay token");
+            //UI Event
+        }
+        else
+        {
+            Debug.Log(Token);
+            Debug.Log(Username);
+            StartCoroutine(GetPerfil());
+            
+        }
+    }
+
+    public void Login()
+    {
+        AuthData data = new AuthData();
+
+        data.username = GameObject.Find("InputFieldUsername").GetComponent<InputField>().text;
+        data.password = GameObject.Find("InputFieldPassword").GetComponent<InputField>().text;
+
+        string postdata = JsonUtility.ToJson(data);
+
+        StartCoroutine(LoginPost(postdata));
+
+    }
+
+    IEnumerator LoginPost(string postData)
+    {
+        string url = URL + "/api/auth/login";
+        UnityWebRequest www = UnityWebRequest.Put(url,postData);
+        www.method = "POST";
+        www.SetRequestHeader("content-type", "application/json");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError)
+        {
+            Debug.Log("NETWORK ERROR: "+www.error);
+        }
+        else
+        {
+           if(www.responseCode == 200)
+            {
+                AuthData resData = JsonUtility.FromJson<AuthData>(www.downloadHandler.text);
+
+                PlayerPrefs.SetString("token", resData.token);
+                PlayerPrefs.SetString("username", resData.usuario.username);
+
+                Debug.Log("Autenticacion exitosa!");
+                SceneManager.LoadScene("Lobbie");
+
+            }
+            else
+            {
+                string mensaje = "Status :" + www.responseCode;
+                mensaje += "\ncontent-type:" + www.GetResponseHeader("content-type");
+                mensaje += "\nError :" + www.error;
+                mensaje+= "\nmsg:" +www.downloadHandler.text;
+                Debug.Log(mensaje);
+
+            }
+        }
+    }
+
+    IEnumerator GetPerfil()
+    {
+        string url = URL + "/api/usuarios/"+ Username;
+        UnityWebRequest www = UnityWebRequest.Get(url);
+        www.SetRequestHeader("x-token", Token);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError)
+        {
+            Debug.Log("NETWORK ERROR: " + www.error);
+        }
+        else
+        {
+            if (www.responseCode == 200)
+            {
+                Debug.Log("Autenticacion exitosa!");
+                SceneManager.LoadScene("Lobbie");
+            }
+            else
+            {
+                string mensaje = "Status :" + www.responseCode;
+                mensaje += "\ncontent-type:" + www.GetResponseHeader("content-type");
+                mensaje += "\nError :" + www.error;
+                mensaje += "\nmsg:" + www.downloadHandler.text;
+                Debug.Log(mensaje);
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class AuthData
+{
+    public string username;
+    public string password;
+    public string msg;
+    public string token;
+    public UserData usuario;
+}
+
+[System.Serializable]
+public class UserData
+{
+    public int _id;
+    public string username;
+    public bool estado;
+}
---
+++ Assets/Scripts/WebSocketManager.cs
@@ -0,0 +1,135 @@
+﻿using Firesplash.UnityAssets.SocketIO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WebSocketManager : MonoBehaviour
+{
+    private SocketIOCommunicator Socket;
+
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        //Socket = GameObject.Find("SocketIO").GetComponent<SocketIOCommunicator>(); 
+        Socket = gameObject.GetComponent<SocketIOCommunicator>();
+    }
+
+    void Start()
+    {
+
+        Socket.Instance.On("connect", (string data) =>
+         {
+             Debug.Log("LOCAL: Conectado al servidor");
+
+         });
+        Socket.Instance.On("disconnect", (string payload) => {
+            if (payload.Equals("io server disconnect"))
+            {
+                Debug.Log("Disconnected from server.");
+            }
+            else
+            {
+                Debug.LogWarning("We have been unexpecteldy disconnected. This will cause an automatic reconnect. Reason: " + payload);
+            }
+        });
+        Socket.Instance.On("friendRequest", (string payload) =>
+        {
+            JSONData data = JsonUtility.FromJson<JSONData>(payload);
+
+            Debug.Log("Solicitud de amistad de " + data.username);
+
+            SendFriendResponse(data.username, true);
+        });
+
+        Socket.Instance.On("friendResponse", (string payload) =>
+        {
+            JSONData data = JsonUtility.FromJson<JSONData>(payload);
+
+            if (data.ok)
+            {
+                Debug.Log(data.msg);
+            }
+            else
+            {
+                Debug.Log(data.msg);
+            }
+            
+        });
+
+        Socket.Instance.On("friendNotOnline", (string payload) =>
+        {
+            JSONData data = JsonUtility.FromJson<JSONData>(payload);
+
+            Debug.Log( data.username+ "no esta en linea, intente mas tarde");
+        });
+        Socket.Instance.On("friendOnline", (string payload) =>
+        {
+            JSONData data = JsonUtility.FromJson<JSONData>(payload);
+
+            Debug.Log("Tu amigo "+data.username + "se ha contecado");
+        });
+
+
+
+        string token = PlayerPrefs.GetString("token");
+        string username = PlayerPrefs.GetString("username");
+
+        if (string.IsNullOrEmpty(token))
+        {
+            Debug.Log("No hay token");
+            SceneManager.LoadScene("Main");
+            return;
+        }
+
+        SIOAuthPayload auth = new SIOAuthPayload();
+        auth.AddElement("username", username); //The server will access this using socket.handshake.auth.id
+        auth.AddElement("token", token); //The server will access this using socket.handshake.auth.token
+        Socket.Instance.Connect(auth);
+
+    }
+
+
+    public void FindMatch()
+    {
+        Debug.Log("LOCAL: FIND MATCH");
+        Socket.Instance.Emit("findMatch");
+    }
+    public void SendFriendRequest( )
+    {
+
+        JSONData data = new JSONData();
+        data.username = GameObject.Find("InputFriendUsername").GetComponent<InputField>().text;
+
+        Socket.Instance.Emit("friendRequest", JsonUtility.ToJson(data),false);
+    }
+
+    public void SendFriendResponse(string username, bool response)
+    {
+
+        JSONData data = new JSONData();
+        data.username = username;
+        data.response = response;
+        Socket.Instance.Emit("friendResponse", JsonUtility.ToJson(data), false);
+    }
+    public void ReceiveFriendRequest()
+    {
+
+    }
+
+    public void FriendNotOnline()
+    {
+
+    }
+   
+}
+
+public class JSONData
+{
+   public string username; 
+   public bool ok; 
+   public string msg;
+   public bool response;
+}
